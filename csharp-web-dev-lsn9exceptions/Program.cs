c924e4f57bab2af0336bab929d2ad24e0e16e6f8
mtorres9095 <mtorres9095@gmail.com>
2020-06-16 13:10:05 -0400

    Chapter 9 Exerecise Completed

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -4,21 +4,53 @@ using System.Collections.Generic;
 namespace csharp_web_dev_lsn9exceptions
 {
     class Program
-    {
+        {
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero!");
+            }
+
+            double value = x / y;
+
+            return value;
         }
+
+    
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            int studentPoints = 0;
+            string value = fileName;
+
+
+            if (value == null || value == "")
+            {
+                throw new ArgumentOutOfRangeException("No Points for no answer");
+            }
+            else if (value.EndsWith(".cs"))
+            {
+                studentPoints = 1;
+            }
+
+            return studentPoints;
         }
 
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            try
+            {
+                Console.WriteLine(Divide(9, 0));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -27,7 +59,26 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
+            foreach (KeyValuePair<string, string> student in students)
+            {
 
+                try
+                {
+                    Console.WriteLine(student.Key + " : " + CheckFileExtension(student.Value));
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+
+
+            }
         }
+
+
     }
 }
+
+
